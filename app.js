af1008d4400b1a068a3bd22b9625c0de2ab886d2
iamclifford321 <iamclifford321@gmail.com>
2024-06-29 21:43:51 +0800

    seventh commit

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const Joi = require('@hapi/joi');
 app.use(express.json());
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    // res.sendFile(path.join(__dirname, 'index.html'))
     // res.send('Welcome to Daily Code Buffer in Heroku Auto Deployments!!');
+    res.send('index.html')
 })
 
 app.get('/test', (req,res) => {
