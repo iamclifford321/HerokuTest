db408ba3bc8d41f4b365f45a96207bf3914a9763
iamclifford321 <iamclifford321@gmail.com>
2024-06-29 21:33:48 +0800

    second commit

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const Joi = require('@hapi/joi');
 app.use(express.json());
 
 app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployments!!');
+    res.sendFile('./index.html', (e) => {
+        if(e){
+            res.status(500).send(e)
+        }
+    })
+    // res.send('Welcome to Daily Code Buffer in Heroku Auto Deployments!!');
 })
+
 app.get('/test', (req,res) => {
     res.send('testset');
 });
