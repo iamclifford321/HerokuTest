712a10d4740c5ea08d8e647815da566ca6d60ffb
iamclifford321 <iamclifford321@gmail.com>
2024-06-30 19:50:36 +0800

    test

---
+++ activity.js
@@ -0,0 +1,14 @@
+var connection = new Postmonger.Session();
+
+connection.trigger('ready');
+
+connection.on('initActivity', function(data){
+    console.log("data: ", data);
+    document.getElementById('configuration').value = JSON.stringify(data, null, 2)
+});
+
+connection.on('clickedNext', function(){
+    var configuration = JSON.parse(document.getElementById('configuration').value);
+    console.log('configuration', configuration)
+    // connection.trigger('updateActivity', configuration);
+});
